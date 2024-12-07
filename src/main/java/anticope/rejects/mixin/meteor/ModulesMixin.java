37238d15c2f7bfff7071a32a37335ae065f434d5
norax0 <125187871+norax0@users.noreply.github.com>
2024-12-08 00:41:00 +0100

    Update ModulesMixin.java

--- src/main/java/anticope/rejects/mixin/meteor/ModulesMixin.java
+++ src/main/java/anticope/rejects/mixin/meteor/ModulesMixin.java
@@ -18,8 +18,11 @@ public class ModuleMixin {
 
     @Mutable @Shadow public String title;
 
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void onInit(Category category, String name, String description, String[] aliases, CallbackInfo info) {
+    @Inject(
+            method = "<init>(Lmeteordevelopment/meteorclient/systems/modules/Category;Ljava/lang/String;Ljava/lang/String;)V",
+            at = @At("TAIL")
+    )
+    private void onInit(Category category, String name, String description, CallbackInfo info) {
         if (RejectsConfig.get().duplicateModuleNames) {
             this.name = RejectsUtils.getModuleName(name);
             this.title = Utils.nameToTitle(this.name);
